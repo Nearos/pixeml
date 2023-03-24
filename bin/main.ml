e39f8e45bcd8511b92fcf33acf6174230df13152
Soren van den Berg <sorenvdbs@yahoo.co.uk>
2023-03-24 17:04:29 -0400

    Caught previously uncaught exception

--- bin/main.ml
+++ bin/main.ml
@@ -6,6 +6,15 @@ let () = print_newline ()
     let* _ = TaskManagerDatabase.restore_from_database mvar [] in 
     Lwt.return () *)
 
+let () = Lwt.async_exception_hook := (function
+  | Unix.Unix_error (error, func, arg) ->
+    Logs.warn (fun m ->
+      m  "Client connection error %s: %s(%S)"
+        (Unix.error_message error) func arg
+    )
+  | exn -> Logs.err (fun m -> m "Unhandled exception: %a" Fmt.exn exn)
+)
+
 let  () = 
     let mvar = Lwt_mvar.create_empty () in
     Lwt_main.run (Lwt.pick [WebServer.server_promise mvar; Scheduler.scheduler mvar])
